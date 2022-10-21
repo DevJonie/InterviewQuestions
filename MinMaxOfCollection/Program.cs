faaa90253e2c25191d075c87e65591a679411bc3
Jonie <john.addo@hubtel.com>
2022-10-21 18:55:47 +0000

    Add MinMaxOfCollection

---
+++ MinMaxOfCollection/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] arr = new int[] { -1, 0, 1, 2, 3, 120, 4, 5, 30, 45, 90 };
+
+//Max of Collection
+//Complexity = O(n)
+int max = int.MinValue;
+foreach (var i in arr)
+    if (i > max) max = i;
+Console.WriteLine("Max: {0}", max);
+
+//Min of Collection
+//Complexity = O(n)
+int min = int.MaxValue;
+foreach (var i in arr)
+    if (i < min) min = i;
+Console.WriteLine("Min: {0}", min);
+
