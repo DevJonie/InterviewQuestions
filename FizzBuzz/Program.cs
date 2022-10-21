811bdc79a5edf8f68b728ef47d451a4a35bd1972
Jonie <john.addo@hubtel.com>
2022-10-21 18:35:41 +0000

    FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] arr = new int[] { -1, 0, 1, 2 ,3, 4, 5, 30, 45, 90};
+
+foreach (int i in arr) 
+{
+    if (i % 15 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+	else if (i % 3 == 0)
+	{
+		Console.WriteLine("Fizz");
+	}
+	else if (i % 5 == 0)
+	{
+		Console.WriteLine("Buzz");
+	}
+	else {
+		Console.WriteLine(i);
+	}
+}
