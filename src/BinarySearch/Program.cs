ba708f0fe03421dba0b5c2f13339c60a1e55fd95
Jonie <john.addo@hubtel.com>
2022-10-22 09:13:01 +0000

    Add Recursion version to binary search

--- src/BinarySearch/Program.cs
+++ src/BinarySearch/Program.cs
@@ -1,11 +1,18 @@
-﻿int[] arr = new int[] { -1, 0, 1, 2, 3, 120, 4, 5, 30, 45, 90, 90, -1 };
+﻿using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+int[] arr = new int[] { -1, 0, 1, 2, 3, 120, 4, 5, 30, 45, 90, 90, -1 };
 Array.Sort(arr);
 PrintArray(arr);
 int item = 120;
-var index = BinarySearch(arr, item);
-Console.WriteLine("Index of {0} = {1}",item,index);
 
-int BinarySearch(int[] arr, int item)
+Console.WriteLine("(While loop) Index of {0} = {1}",item, BinarySearchWhileLoop(arr, item));
+
+Console.WriteLine("(Recursion) Index of {0} = {1}",item, BinarySearchWithRecursion(arr, item));
+
+//Using While loop
+//Complexity O(log n)
+int BinarySearchWhileLoop(int[] arr, int item)
 {
     int low = 0;
     int high = arr.Length - 1;
@@ -21,6 +28,30 @@ int BinarySearch(int[] arr, int item)
 	}
 	return -1;
 }
+
+
+//Using Recursion
+int BinarySearchWithRecursion(int[] arr, int item)
+{
+    return BinarySearch(arr, item, 0, arr.Length - 1);
+
+    int BinarySearch(int[] arr, int item, int low, int high)
+    {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == item)
+            return mid;
+
+		if (mid == arr.Length - 1 || mid == 0)
+			return -1;
+
+        if (arr[mid] < item)
+            return BinarySearch(arr, item, mid + 1, high);
+        else
+            return BinarySearch(arr, item, low, mid - 1);
+    }
+}
+
 
 void PrintArray(int[] arr) 
 {
