29328a7f79530b651fbaa9fcff3f402cb5a6eb2f
Jonie <john.addo@hubtel.com>
2022-10-27 21:16:59 +0000

    Add TowerBreakers

---
+++ src/TowerBreakers/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * 
+ Two players are playing a game of Tower Breakers! Player
+
+always moves first, and both players always play optimally.The rules of the game are as follows:
+
+    Initially there are 
+
+towers.
+Each tower is of height
+.
+The players move in alternating turns.
+In each turn, a player can choose a tower of height
+and reduce its height to , where and evenly divides
+
+    .
+    If the current player is unable to make a move, they lose the game.
+
+Given the values of
+and , determine which player will win. If the first player wins, return . Otherwise, return . 
+*/
+
+
+Console.WriteLine(TowerBreakers(3, 4));
+
+
+static int TowerBreakers(int n, int m)
+{
+    if (m == 1 || n%2 == 0)
+    {
+        return 2;
+    }
+    return 1;
+}
+
