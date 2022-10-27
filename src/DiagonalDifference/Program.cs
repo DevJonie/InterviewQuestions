c733e2465062a7a076c18a57b27272566a9fdc81
Jonie <john.addo@hubtel.com>
2022-10-27 20:37:24 +0000

    Add Diagonal Difference

---
+++ src/DiagonalDifference/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ Given a square matrix, calculate the absolute difference between the sums of its diagonals. 
+
+*/
+
+
+Console.WriteLine(DiagonalDifference(new List<List<int>> 
+{
+    new List<int>{1,2,3 },
+    new List<int>{4,5,6 },
+    new List<int>{9,8,9 },
+}));
+
+static int DiagonalDifference(List<List<int>> arr)
+{
+    int ld = 0;
+    int rd = 0;
+    int size = arr.Count() - 1;
+
+    for (int i = 0; i <= size; i++)
+    {
+        ld += arr[i][i];
+        rd += arr[i][size - i];
+    }
+
+    return Math.Abs(ld - rd);
+}
