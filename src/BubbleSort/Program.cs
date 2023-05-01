59e2ff528fb0cbe3e94351172a4b81fa1b7eb9d5
Jonie <john.addo@hubtel.com>
2023-05-01 07:02:40 +0000

    Implement Bubble sort

---
+++ src/BubbleSort/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] arr = new int[] { 5, 1, 4, 2, 8 };
+
+Console.WriteLine($"Unsorted array: {string.Join(", ", arr)}");
+
+for (int i = 0; i < arr.Length; i++)
+{
+    bool swaped = false;
+    for (int j = 0; j < arr.Length - 1; j++)
+    {
+        if (arr[j] > arr[j + 1])
+        {
+            int temp = arr[j + 1];
+            arr[j + 1] = arr[j];
+            arr[j] = temp;
+            swaped = true;
+        }
+    }
+
+    if (!swaped)
+    {
+        break;
+    }
+}
+
+Console.WriteLine($"Sorted array: {string.Join(", ", arr)}");
