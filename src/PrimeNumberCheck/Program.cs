abdd334fec113cc9331c66fc5546aac5aa27be51
Jonie <john.addo@hubtel.com>
2023-05-04 10:25:42 +0000

    Prime Number

---
+++ src/PrimeNumberCheck/Program.cs
@@ -0,0 +1,35 @@
+﻿while (true)
+{
+    try
+    {
+        Console.Write("Please enter a number: ");
+        var n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"{n} is {(IsPrime(n) ? "Prime" : "Not prime")}");
+    }
+    catch 
+    {
+        Console.WriteLine("ERROR: Invalid input.");
+    }
+}
+
+static bool IsPrime(int n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+
+    if (n == 2)
+    {
+        return true;
+    }
+
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
